0e3c6b965cf7f29a3a7929d255b1527cf7d4c9e6
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-04-26 12:17:46 -0400

    initial commit

---
+++ db/seeds.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const { Creature } = require('./db/schema')
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+const saved = async () => {
+    await Creature.remove()
+    const luke = new Creature({ name: 'Luke', description: 'Jedi' })
+    await luke.save()
+    const darth = new Creature({ name: 'Darth Vader', description: 'Father of Luke' })
+    await darth.save()
+    db.close()
+}
+saved()
